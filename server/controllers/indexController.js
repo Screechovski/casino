06703f7f4e632aaa1b3a26bc4422c47ed722ed7b
Yaroslav <yaroslav_dmitriev@guarantpay.com>
2022-10-07 05:22:01 +0300

    fix no such file prod error; build for prod

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -3,7 +3,7 @@ import path from 'path';
 export const indexController = (req, res) => {
   try {
     if (/\.(js|ico|css|jpe?g|png|webp|svg)$/i.test(req.originalUrl)) {
-      res.sendFile(path.join(__dirname, req.originalUrl));
+      res.sendFile(path.join(__dirname, req.originalUrl.replace(/\/casino/, '')));
     } else {
       res.sendFile(path.join(__dirname, '/index.html'));
     }
